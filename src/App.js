8a949eefb3cfc9375dcf5de3a48bee5c9d6142a1
RAZEX0LOL <khattayev00@mail.ru>
2023-10-07 14:03:19 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/global/Header";
+import Footer from "./components/global/Footer";
+import Items from "./components/global/Items";
+import Categories from "./components/global/Categories";
+import ShowFullItem from "./components/global/ShowFullItem";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [orders, setOrders] = useState([]);
+  const [currentItems, setCurrentItems] = useState([]); // Начальное значение пустое []
+  const [items, setItems] = useState([]);
+  const [showFullItem, setShowFullItem] = useState(false);
+  const [fullItem, setFullItem] = useState({});
+
+  useEffect(() => {
+    axios
+        .get("http://localhost:3001/items")
+        .then((response) => {
+          setItems(response.data);
+          chooseCategory("all");
+          setCurrentItems(response.data); // Устанавливаем текущие элементы сразу после загрузки
+        })
+        .catch((error) => {
+          console.error("Ошибка при загрузке данных:", error);
+        });
+  }, []);
+
+  // Функция по добавлению вещей в корзину
+  const addToOrder = (item) => {
+    if (!orders.some((el) => el.id === item.id)) {
+      setOrders([...orders, item]);
+    }
+  };
+
+  // Функция по удалению вещей из корзины
+  const deleteOrder = (id) => {
+    setOrders(orders.filter((el) => el.id !== id));
+  };
+
+  // Функция для выбора категории
+  const chooseCategory = (category) => {
+    if (category === "all") {
+      setCurrentItems(items);
+    } else {
+      setCurrentItems(items.filter((el) => el.category === category));
+    }
+  };
+
+  // Функция для отображения полной информации об элементе
+  const onShowItem = (item) => {
+    setFullItem(item);
+    setShowFullItem(!showFullItem);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"wrapper"}>
+        <Header orders={orders} onDelete={deleteOrder} />
+        <Categories chooseCategory={chooseCategory} />
+        <Items onShowItem={onShowItem} items={currentItems} onAdd={addToOrder} />
+
+        {showFullItem && (
+            <ShowFullItem onAdd={addToOrder} onShowItem={onShowItem} item={fullItem} />
+        )}
+        <Footer />
+      </div>
   );
 }
 
-export default App;
